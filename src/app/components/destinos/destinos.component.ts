7cf2bf9562982d508eedac1e8816072bf2fe79fe
Maka-1019 <jkamila.amaya10@gmail.com>
2025-05-25 13:14:28 -0500

    Se modifica html destinos

--- src/app/components/destinos/destinos.component.ts
+++ src/app/components/destinos/destinos.component.ts
@@ -10,12 +10,35 @@ import { CommonModule } from '@angular/common';
 })
 export class DestinosComponent implements OnInit {
   destinos: Destino[] = [];
+  temaActual: 'claro' | 'oscuro' = 'claro';
 
   constructor(private destinosService: DestinosService) { }
 
   ngOnInit(): void {
     this.destinosService.getDestinos().subscribe((data: Destino[]) => {
+      console.log('Datos recibidos:', data);
       this.destinos = data;
     });
+
+ // Detectar tema guardado
+    const temaGuardado = localStorage.getItem('tema');
+  this.temaActual = (temaGuardado as 'claro' | 'oscuro') || 'claro';
+  this.aplicarTema();
+}
+
+ alternarTema(): void {
+  this.temaActual = this.temaActual === 'claro' ? 'oscuro' : 'claro';
+  localStorage.setItem('tema', this.temaActual);
+  this.aplicarTema();
+}
+
+ aplicarTema(): void {
+  const body = document.body;
+  if (this.temaActual === 'oscuro') {
+    body.classList.add('modo-oscuro');
+  } else {
+    body.classList.remove('modo-oscuro');
+  }
+
   }
 }
