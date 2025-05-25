388baf7b07cdf8c0af46ebbf4693a59075c82fb9
Juan <juans.escarragal@konradlorenz.edu.co>
2025-05-25 14:31:54 -0500

    creacion componente perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
